b8e2f6aad9af8af6844cace02530dd37ee246959
firemek <75761979+firemek@users.noreply.github.com>
2024-10-23 14:40:54 -0400

    this is the encoded addition to the repo

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-# this is my first github addition
-print("hello world")
+
+def encode(password):
+    temp = []
+    output = ""
+    for i in range(0, len(password)):  # for every character in the password
+        temp.append(int(password[i])) # adds it to temp list
+    for a in range(0, len(temp)):  # for every item in the temp list
+        temp[a] = temp[a] + 3  # adds 3 to every number
+        output += str(temp[a])  # adds every item in the list into output string
+
+    return output
+
+
+if __name__ == '__main__':
+    x = 0  # variable to start
+
+    while x == 0:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")  # prints out menu
+        user_selection = int(input("\nPlease enter an option: "))  # prompts user for menu selection
+        user_password = input("Please enter your password to encode: ") # prompts user for the password to encode
+
+        if user_selection == 1:  # runs user_password into encode function
+            user_password = encode(user_password)
+            print("Your password has been encoded and stored!")
+
+        elif user_selection == 2:  # runs user_password into decode function
+            # TODO: function to decode
+            pass
+
+        elif user_selection == 3: # terminates the program
+            x = 1
